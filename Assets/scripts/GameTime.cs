815605da657418f834944a0e2458def0b9f3727c
Tu Tily <1114928019@qq.com>
2019-09-19 16:53:25 -0400

    initial commit

---
+++ Assets/scripts/GameTime.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameTime : MonoBehaviour
+{
+    private float timer = 120f;
+    LevelManager manager;
+    private void Start()
+    {
+        
+        manager = GameObject.Find("GameControl").GetComponent<LevelManager>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer < 0)
+        {
+            manager.lose();
+            Destroy(this.gameObject);
+
+        }
+    }
+}
---
+++ Assets/scripts/LevelManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+    public GameObject winImage;
+    public GameObject loseImage;
+    //for now, we'll only control winning or losing here.
+    //in the future, you can add a score count here, a timer,
+    //high scores,etc.
+    public int count = 0;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void lose(){
+        loseImage.SetActive(true);
+        Time.timeScale = 0;
+    }
+    public void win(){
+        winImage.SetActive(true);
+        Time.timeScale = 0;
+    }
+}
